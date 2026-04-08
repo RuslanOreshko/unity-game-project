fce72b7120e1d1e60c01c0de2b4f64b52e54d386
RuslanOreshko <3na3drocher@gmail.com>
2026-04-09 00:50:22 +0300

    feat: add player movement, jump and basic sprite setup

---
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInputHandler))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private PlayerInputHandler input;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        input = GetComponent<PlayerInputHandler>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(input.MoveInput.x * moveSpeed, rb.linearVelocity.y);
+    }
+}
